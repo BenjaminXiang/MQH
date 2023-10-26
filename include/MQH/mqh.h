2399f87baf29c500a0b7b59a2e720878d3aa100e
longxiang <xiangl3@mail.sustech.edu.cn>
2023-10-26 11:42:51 +0800

    Update

---
+++ include/MQH/mqh.h
@@ -0,0 +1,38 @@
+#include <utils/io_utils.h>
+#include <utils/timer.h>
+
+#include <iostream>
+#include <fstream>
+#include <queue>
+#include <chrono>
+#include <vector>
+#include "visited_list_pool.h"
+#include <cmath>
+#include <cassert>
+#include <unordered_set>
+#include <algorithm>
+#include <limits.h>
+
+void index(
+    int, 
+    int,
+    int,
+    const std::string&,
+    const std::string&);
+
+void index(int , int , int , char* , char* );
+
+void search(int n_pts,
+            int n_query,
+			int d,
+			int topk,
+			float delta,
+			int l0,
+			int flag_,
+			const std::string kFrontPath,
+			const std::string kDataset);
+
+void search(int , int , int , int , float , int , int,  char* , char* , char* , char* );
+
+void ground_truth(int , int , int , char* , char* , char* );
+
