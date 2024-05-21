eaef470ec40a80dc6ccf70030f0dc09469894135
BenjaminXiang <xiangl3@mail.sustech.edu.cn>
2024-05-21 20:15:33 +0800

    Tmp Commit

--- include/MQH/mqh.h
+++ include/MQH/mqh.h
@@ -1,48 +1,37 @@
+#include <limits.h>
 #include <utils/io_utils.h>
 #include <utils/timer.h>
 
-#include <iostream>
-#include <fstream>
-#include <queue>
-#include <chrono>
-#include <vector>
-#include "visited_list_pool.h"
-#include <cmath>
-#include <cassert>
-#include <unordered_set>
 #include <algorithm>
-#include <limits.h>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <queue>
+#include <string_view>
+#include <unordered_set>
+#include <vector>
+
+#include "visited_list_pool.h"
 
 // void index(
-//     int, 
+//     int,
 //     int,
 //     int,
 //     const std::string&,
 //     const std::string&);
 
-void index(
-	int n_pts, 
-	int d,
-	int n_sample,
-	const std::string& data_path,
-	const std::string& data_type,
-	const std::string& index_data);
+void index(int n_pts, int d, int n_sample, const float* data_ptr, const std::string& index_data);
 
-void index(int , int , int , char* , char* );
+void index(int n_pts, int d, int n_sample, const std::string& data_path, const std::string& data_type,
+           const std::string& index_data);
 
-void search(int n_pts,
-            int n_query,
-			int d,
-			int topk,
-			float delta,
-			int l0,
-			int flag_,
-			const std::string& kFrontPath,
-			const std::string& kDataset,
-			const std::string& kDataType,
-			const std::string& kQueryType);
+void index(int, int, int, char*, char*);
 
-void search(int , int , int , int , float , int , int,  char* , char* , char* , char* );
+void search(int n_pts, int n_query, int d, int topk, float delta, int l0, int flag_, const std::string& kFrontPath,
+            const std::string& kDataset, const std::string& kDataType, const std::string& kQueryType);
 
-void ground_truth(int , int , int , char* , char* , char* );
+void search(int, int, int, int, float, int, int, char*, char*, char*, char*);
 
+void ground_truth(int, int, int, char*, char*, char*);
