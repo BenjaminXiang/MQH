d12a66ce5cae53aad769929be61686cfd29363a6
BenjaminXiang <xiangl3@mail.sustech.edu.cn>
2024-05-24 18:30:04 +0800

    Update

--- include/MQH/mqh.h
+++ include/MQH/mqh.h
@@ -24,14 +24,17 @@
 
 void index(int n_pts, int d, int n_sample, const float* data_ptr, const std::string& index_data);
 
-void index(int n_pts, int d, int n_sample, const std::string& data_path, const std::string& data_type,
-           const std::string& index_data);
+// void index(int n_pts, int d, int n_sample, const std::string& data_path, const std::string& data_type,
+//            const std::string& index_data);
 
-void index(int, int, int, char*, char*);
+// void index(int, int, int, char*, char*);
 
-void search(int n_pts, int n_query, int d, int topk, float delta, int l0, int flag_, const std::string& kFrontPath,
-            const std::string& kDataset, const std::string& kDataType, const std::string& kQueryType);
+void search(int n_pts, int n_query, int d, int topk, float delta, int l0, int flag_, const float* query_ptr,
+            const float* data_ptr, std::vector<std::vector<unsigned>>& gt, const char* path_index);
 
-void search(int, int, int, int, float, int, int, char*, char*, char*, char*);
+// void search(int n_pts, int n_query, int d, int topk, float delta, int l0, int flag_, const std::string& kFrontPath,
+//             const std::string& kDataset, const std::string& kDataType, const std::string& kQueryType);
+
+// void search(int, int, int, int, float, int, int, char*, char*, char*, char*);
 
 void ground_truth(int, int, int, char*, char*, char*);
